9aa1dc5943e4262fd26be2f4cd3f4a0abb0f0a96
Daniel <danielwebd17@gmail.com>
2025-04-07 21:30:55 +0600

    Refactor views and urls of core app

--- backend_api/core/urls.py
+++ backend_api/core/urls.py
@@ -4,9 +4,10 @@ Module for urls configuration of core app
 
 from django.urls import path    
 
-from .views import home
+from .views import home, todos_list
 
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('todos', todos_list, name="todos_list")
 ]
